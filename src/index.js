bc81e9d6ffe0b715f9e4b48a032c73e389c4eea5
Nicholas Mutale <nicomutale@gmail.com>
2022-03-02 20:55:11 +0200

    update index js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
-import './style.css';
+// import './style.css';
 
 const tDList = document.querySelector('.tDList');
+document.querySelector('#submit').disabled = true;
+document.querySelector('.inputTask').onkeyup = () => {
+  document.querySelector('#submit').disabled = false;
+};
 
-const arrTdlist = [
-  {
-    id: 1,
-    tdlist: 'Repair my laptop',
-    complete: false,
-  },
-  {
-    id: 1,
-    tdlist: 'Clean the yard',
-    complete: false,
-  },
-  {
-    id: 1,
-    tdlist: 'Study javScript',
-    complete: false,
-  },
-];
-
-for (let i = 0; i < arrTdlist.length; i += 1) {
-  tDList.innerHTML = `
-    ${tDList.innerHTML}
-    <li class="displayTdl">
-    <div class="tdldisplayContainer"><input type='checkbox' class='checkbox'>
-    <p class='tdText' id=${arrTdlist[i].id}> ${arrTdlist[i].tdlist}</p>
-    </div>
-    <span id='delete'><i class='fa-solid fa-ellipsis-vertical'></i></span>
-    </li>
-    `;
+// form submission
+document.querySelector('form').onsubmit = () => {
+  const task = document.querySelector('.inputTask').value;
+  const li = document.createElement('li');
+  document.querySelector('.inputTask').value = '';
+  li.innerHTML = task;
+  tDList.append(li);
+  return false;
 }
