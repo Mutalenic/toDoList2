c4fb60c44898a89ef22f4e29f692085d1bf80e10
Nicholas Mutale <nicomutale@gmail.com>
2022-03-02 13:59:20 +0200

    update index js update style css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import './style.css';
-import component from './modules/component.js';
 
-window.onload = component();
+const tDList = document.querySelector('.tDList');
+
+const arrTdlist = [
+  {
+    id: 1,
+    tdlist: 'Repair my laptop',
+    complete: false,
+  },
+  {
+    id: 1,
+    tdlist: 'Clean the yard',
+    complete: false,
+  },
+  {
+    id: 1,
+    tdlist: 'Study javScript',
+    complete: false,
+  },
+];
+
+for (let i = 0; i < arrTdlist.length; i += 1) {
+  tDList.innerHTML = `
+    ${tDList.innerHTML}
+    <li class="displayTdl">
+    <div class="tdldisplayContainer"><input type='checkbox' class='checkbox'>
+    <p class='tdText' id=${arrTdlist[i].id}> ${arrTdlist[i].tdlist}</p>
+    </div>
+    <span id='delete'><i class='fa-solid fa-ellipsis-vertical'></i></span>
+    </li>
+    `;
+}
