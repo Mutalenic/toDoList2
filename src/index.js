1f1437947712a5e27b3213f373a61dc6e373daa3
Nicholas Mutale <nicomutale@gmail.com>
2022-03-04 18:45:15 +0200

    update app js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import { getTask } from './data.js';
-import { deleteTask, deleteOne, updateTask } from './controllTools.js';
+import { deleteTask, deleteOne, updateTask } from './updateApp.js';
 
 export const taskList = document.querySelector('.task-list-container');
 
