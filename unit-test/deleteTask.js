c6259ecb68e110bf1c43d16972e54891f69fbb19
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-03-10 15:27:57 +0100

    Delete function test done

--- unit-test/deleteTask.js
+++ unit-test/deleteTask.js
@@ -1,8 +1,9 @@
 const deleteTask = (taskList, index) => {
-  const newTaskList = taskList;
+  const newTaskList = [...taskList];
   if (index > -1 && index < newTaskList.length) {
     newTaskList.splice(index,1)
   }
+  return newTaskList
 };
 
 export default deleteTask;
